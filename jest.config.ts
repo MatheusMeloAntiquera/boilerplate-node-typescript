bace6c41b7411c559edbb13e6f1fbd9087327939
Matheus Melo Antiquera <matheusantiquera@finchsolucoes.com.br>
2022-03-06 23:32:16 -0300

    test: :stethoscope: ignore interfaces and index.ts on coverage

--- jest.config.ts
+++ jest.config.ts
@@ -26,9 +26,11 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "interfaces",
+    "<rootDir>/src/index.ts",
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
